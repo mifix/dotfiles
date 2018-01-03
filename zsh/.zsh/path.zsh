2f55bc7040bd476bd78fdd226cebd22a73691c74
Christian Mayer <gitlab@mifix.com>
2018-01-03 07:34:58 +0100

    fix s**t

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,13 +1,13 @@
 
 # check for custom bin directory and add to path
 if [[ -d ~/bin ]]; then
-    export PATH=~/bin
+    export PATH=$PATH:~/bin
 fi
 
 [ -f ~/.nix-profile/etc/profile.d/nix.sh ] && source ~/.nix-profile/etc/profile.d/nix.sh
 
 # Snap
-export PATH=$PAHT:/snap/bin
+export PATH=$PATH:/snap/bin
 
 # GO
 export GOPATH=$HOME/Projects/go

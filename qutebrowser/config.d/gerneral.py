a29a1513fe5aaea97878a7255ae6ac6f095d8a11
Christian Mayer <gitlab@mifix.com>
2018-05-22 08:10:22 +0200

    [qutebrowser] tab settings

--- qutebrowser/config.d/gerneral.py
+++ qutebrowser/config.d/gerneral.py
@@ -16,3 +16,16 @@ c.scrolling.smooth = True
 ## Type: ShellCommand
 # c.editor.command = ['gvim', '-f', '{file}', '-c', 'normal {line}G{column0}l']
 c.editor.command =  ["nvim-gui", "-f", "{file}", "-c", "'normal {line}G{column0}l'"]
+
+
+## Open new tabs (middleclick/ctrl+click) in the background.
+## Type: Bool
+c.tabs.background = True
+
+## Which tab to select when the focused tab is removed.
+## Type: SelectOnRemove
+## Valid values:
+##   - prev: Select the tab which came before the closed one (left in horizontal, above in vertical).
+##   - next: Select the tab which came after the closed one (right in horizontal, below in vertical).
+##   - last-used: Select the previously selected tab.
+c.tabs.select_on_remove = 'last-used'

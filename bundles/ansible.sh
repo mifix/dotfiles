e6ebf96439929cf028166d72aa1d9958c1db8d68
Christian Mayer <github@mifix.com>
2019-02-03 11:54:34 +0100

    add ansible bundle

---
+++ bundles/ansible.sh
@@ -0,0 +1,11 @@
+APT_PPA_REPOSITORIES=(ansible/ansible)
+
+APT_PACKAGES=(ansible)
+
+# -----------------------------------------------------------------------
+
+header "Installing Ansible..."
+
+
+install_packages "APT"
+

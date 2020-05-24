ebe366fcb24be64252b55a8d010b2bc1a59b9261
Christian Mayer <gitlab@mifix.com>
2020-05-24 16:40:30 +0200

    update bundels

--- bundles/ansible.sh
+++ bundles/ansible.sh
@@ -9,3 +9,6 @@ header "Installing Ansible..."
 
 install_packages "APT"
 
+
+task "Installing molecule" "pip2 install --user molecule" "$HOME/.local/bin/molecule"
+task "Installing ansible-generator" "pip2 install --user ansible-generator" "$HOME/.local/bin/ansible-generate"
--- bundles/extended_tui.sh
+++ bundles/extended_tui.sh
@@ -1,6 +1,6 @@
-APT_PPA_REPOSITORIES=(amanusk/python-s-tui)
+# APT_PPA_REPOSITORIES=(amanusk/python-s-tui)
 
-APT_PACKAGES=(python3-s-tui)
+APT_PACKAGES=(s-tui ranger)
 
 # -----------------------------------------------------------------------------------
 
@@ -10,7 +10,7 @@ header "Installing extended command line apps..."
 install_packages "APT"
 
 
-BAT_VERSION=0.7.1
+BAT_VERSION=0.12.1
 BAT_CURRENT_VERSION=$(bat --version 2>/dev/null | awk '{ print $2 }')
-BAT_INSTALL_CMD="wget -q -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v$BAT_VERSION/bat_$BAT_VERSION_amd64.deb && sudo dpkg -i /tmp/bat.deb"
+BAT_INSTALL_CMD="wget -q -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_amd64.deb && sudo dpkg -i /tmp/bat.deb"
 task "Downloading and installing bat version $BAT_VERSION" "${BAT_INSTALL_CMD}" $BAT_CURRENT_VERSION $BAT_VERSION
